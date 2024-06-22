1bc18a8c95b44da05b948f8b146171f00b4c2d0f
Sa Đam <20521154@gm.uit.edu.vn>
2024-06-22 14:44:23 +0700

    delete chat

--- src/layouts/components/Sidebar/index.jsx
+++ src/layouts/components/Sidebar/index.jsx
@@ -96,30 +96,6 @@ const groupMenus = [
             },
         ],
     },
-<<<<<<< HEAD
-    // {
-    //     main: {
-    //         iconClassname: 'fa-solid fa-tag',
-    //         text: 'Phiếu giảm giá',
-    //         link: '/coupon',
-    //     },
-    //     children: [
-    //         {
-    //             iconClassname: 'fa-solid fa-list',
-    //             text: 'Danh sách',
-    //             link: '/',
-    //             functionName: 'coupon/view',
-    //         },
-    //         {
-    //             iconClassname: 'fa-solid fa-circle-plus',
-    //             text: 'Thêm',
-    //             link: '/add',
-    //             functionName: 'coupon/add',
-    //         },
-    //     ],
-    // },
-=======
->>>>>>> 017f6610ae30c53b9f49f51f89fabf2aeb7d2d02
     {
         main: {
             iconClassname: 'fa-solid fa-tag',
@@ -162,20 +138,7 @@ const groupMenus = [
             },
         ],
     },
-    {
-        main: {
-            iconClassname: 'fa-solid fa-tag',
-            text: 'Chat',
-            link: '/chat',
-        },
-    },
-    // {
-    //     main: {
-    //         iconClassname: 'fa-solid fa-tag',
-    //         text: 'Chat',
-    //         link: '/chat',
-    //     },
-    // },
+
     {
         main: {
             iconClassname: 'fa-solid fa-users',
@@ -264,24 +227,15 @@ const groupMenus = [
 
 function Sidebar() {
     return (
-        <div className="h-full min-w-[240px] bg-blue-500">
-            <header className="flex h-20 w-full flex-col items-center justify-center border-b border-white/40 text-white">
-                <div className="text-lg font-extrabold">QUẢN LÝ</div>
-                <div className="font-bold">CH THỰC PHẨM SẠCH</div>
+        <div className='h-full min-w-[240px] bg-blue-500'>
+            <header className='flex h-20 w-full flex-col items-center justify-center border-b border-white/40 text-white'>
+                <div className='text-lg font-extrabold'>QUẢN LÝ</div>
+                <div className='font-bold'>CH THỰC PHẨM SẠCH</div>
             </header>
 
-            <div className="">
-                <Scrollbars
-                    autoHide
-                    autoHideTimeout={4000}
-                    autoHeight
-                    autoHideDuration={200}
-                    autoHeightMin={`calc(100vh - 80px)`}
-                >
-                    <ul
-                        className="flex h-full flex-col space-y-0.5 p-2"
-                        style={{ overflowY: 'overlay' }}
-                    >
+            <div className=''>
+                <Scrollbars autoHide autoHideTimeout={4000} autoHeight autoHideDuration={200} autoHeightMin={`calc(100vh - 80px)`}>
+                    <ul className='flex h-full flex-col space-y-0.5 p-2' style={{ overflowY: 'overlay' }}>
                         {groupMenus.map((groupMenu, index) => (
                             <GroupMenu key={index} groupMenu={groupMenu} />
                         ))}
